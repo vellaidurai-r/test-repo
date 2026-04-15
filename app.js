c319f65602a51e497a83cadccb0d4e7f332749fa
Vellaidurai <white@Vellaidurais-Mac-mini.local>
2026-04-15 22:50:26 +0530

    Content changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World is awesome! - Node.js CI/CD Pipeline on AWS ECS');
+  res.send('Hello World is awesome and Great! - Node.js CI/CD Pipeline on AWS ECS');
 });
 
 app.listen(port, '0.0.0.0', () => {
